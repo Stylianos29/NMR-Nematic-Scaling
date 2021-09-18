2d6fadf02b2fb8b5c44bf29a7fecc0e0ebfa713e
Stylianos Gregoriou <gregoris@purdue.edu>
2021-09-18 13:30:18 -0400

    Just before I begin with fitting

--- Cuve_fitting.py
+++ Cuve_fitting.py
@@ -13,7 +13,13 @@ spec = importlib.util.spec_from_file_location("Fitting_functions", "NMR-Nematic-
 fit = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(fit)
 
-fit.stretched_exponential(1,1,1,1,1)
+import matplotlib.font_manager as fm
+import pylab
+
+# Edit the font, font size, and axes width
+# plt.rcParams['font.family'] = 'Avenir'
+# plt.rcParams['font.size'] = 18
+# plt.rcParams['axes.linewidth'] = 2
 
 ###################################################################################
 #TEMPORARY
@@ -42,18 +48,25 @@ for temperature_value in Magnetization_recovery_dict.keys():
     x = x[array_indices[::1]]
     y = y[array_indices[::1]]
 
+    #Normalized data
+    y = ( y - min(y) ) / ( max(y) - min(y) )
+
     print("Saving plot for T="+str(temperature_value))
     fig, ax = plt.subplots()
-    ax.scatter(x, y, s=20, color='#00b3b3', label='Data')
+    ax.scatter(x, y, s=20, color='black', label='Data')
     ax.set_xscale('log')
     # pars, cov = curve_fit(f=fit.exponential, xdata=x, ydata=y, p0=[max(y), 1-min(y)/max(y), 100], bounds=(-np.inf, np.inf))
     # stdevs = np.sqrt(np.diag(abs(cov)))
     # res = y - fit.exponential(x, pars[0], pars[1], pars[2])
     # ax.plot(x, fit.exponential(x, pars[0], pars[1], pars[2]), linestyle='--', linewidth=2, color='black')
     
-    pars, cov = curve_fit(f=fit.stretched_exponential, xdata=x, ydata=y, p0=[max(y), 1-min(y)/max(y), 100, 0.5], bounds=(-np.inf, np.inf))
-    ax.plot(x, fit.stretched_exponential(x, pars[0], pars[1], pars[2], pars[3]), linestyle='-.', linewidth=2, color='cyan')
+    pars, cov = curve_fit(f=fit.reduced_stretched_exponential, xdata=x, ydata=y, p0=[100, 0.5], bounds=(-np.inf, np.inf))
+    ax.plot(x, fit.reduced_stretched_exponential(x, *pars), linestyle=':', linewidth=2, color='red')
     
     ax.set(xlabel='Recovery time (s)', ylabel='Magnetization', title='Magnetization Curve at T = '+str(temperature_value)+'K',xscale = "log")
-    plt.legend(['Stretched Exponential with β = {:.2f}'.format(pars[3])], loc="upper left")
-    plt.savefig('NMR-Nematic-Scaling\Output_Files\Fits_LogLinear_Plots\Magnetization_Recovery_Curve_T='+str(temperature_value)+'K_x=0.05898_H=11.7T_Curve_LogLinearPlot.png')
+    ax.set_xlabel('Wavelength (nm)', labelpad=10)
+    ax.set_ylabel('Absorbance (O.D.)', labelpad=10)
+    plt.legend(['Stretched Exponential with β = {:.2f}'.format(pars[1])], loc="upper left")
+    plt.savefig('NMR-Nematic-Scaling\Output_Files\Fits_LogLinear_Plots_Normalized\Magnetization_Recovery_Curve_T='+str(temperature_value)+'K_x=0.05898_H=11.7T_Curve_LogLinearPlot.png')
+
+
--- Fitting_functions.py
+++ Fitting_functions.py
@@ -5,8 +5,11 @@ def testmod():
 
 #Simple exponential form
 def exponential(x, M0, phi, alpha):
-    result = M0*(1-phi*np.exp(-alpha*x))
-    return result
+    return M0*(1-phi*np.exp(-alpha*x))
+
+#Simplified Stretched exponential form
+def reduced_stretched_exponential(x, alpha, beta):
+    return 1-np.exp(-alpha*x)**beta
 
 #Stretched exponential form
 def stretched_exponential(x, M0, phi, alpha, beta):
